26d8ad7b15bb6369a6b71adeadd729e3a761e975
CLIBOT <MR.YAVAR0@GMAIL.COM>
2017-03-15 19:14:51 +0330

    Add files via upload

---
+++ plugins/boradcast.lua
@@ -0,0 +1,41 @@
+--[[
+|------------------------------------------------- |--------- ______-----------------_______---|
+|   ______   __   ______    _____     _____    __  |  _____  |  ____|  __     __    /  _____/  |
+|  |__  __| |  | |__  __|  /     \   |     \  |  | | |__   | | |____  |  |   |  |  /  /____    |
+|    |  |   |  |   |  |   /  /_\  \  |  |\  \ |  | |   /  /  |  ____| |  |   |  |  \____   /   |
+|    |  |   |  |   |  |  /  _____  \ |  | \  \|  | |  /  /_  | |____  |  |___|  |   ___/  /    |
+|    |__|   |__|   |__| /__/     \__\|__|  \_____| | |_____| |______|  \_______/  /______/     |
+|--------------------------------------------------|-------------------------------------------|
+| This Project Powered by : Pouya Poorrahman CopyRight 2016 Jove Version 4.0 Anti Spam Cli Bot |
+|----------------------------------------------------------------------------------------------|
+]]
+local function run(msg, matches)
+	if matches[1] == 'bc' and is_admin1(msg) then
+		local response = matches[3]
+		--send_large_msg("chat#id"..matches[2], response)
+		send_large_msg("channel#id"..matches[2], response)
+	end
+	if matches[1] == 'broadcast' then
+		if is_sudo(msg) then -- Only sudo !
+			local data = load_data(_config.moderation.data)
+			local groups = 'groups'
+			local response = matches[2]
+			for k,v in pairs(data[tostring(groups)]) do
+				chat_id =  v
+				local chat = 'chat#id'..chat_id
+				local channel = 'channel#id'..chat_id
+				send_large_msg(chat, response)
+				send_large_msg(channel, response)
+			end
+		end
+	end
+end
+return {
+  patterns = {
+    "^[#!/](broadcast) (.*)$",
+    "^[#!/](bc) (%d+) (.*)$",
+        "^(broadcast) (.*)$",
+    "^(bc) (%d+) (.*)$"
+  },
+  run = run
+}
